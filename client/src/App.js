ca8f5d074c036fb743e7de0fc3c356f27453e0ab
kevenskk <x5q48@students.keele.ac.uk>
2024-10-18 10:36:09 +0100

    express

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, {useState} from 'react';
+
+
+
+
+
 
 function App() {
+
+
+  const [message, setMessage] = useState("")
+
+  function callAPI() {  
+
+    axios.get('https://tfttracker-server.vercel.app')
+    .then(res => setMessage(res.data))
+    .catch(error => console.log(error))
+
+    
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+
+       <button onClick={callAPI}>Call API</button>
+       <p> {message} </p>
       </header>
     </div>
   );
