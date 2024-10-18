21a8c1574d51ea1c579981a3c328fa31d317167a
kevenskk <x5q48@students.keele.ac.uk>
2024-10-18 11:49:54 +0100

    api key updated

--- server/index.js
+++ server/index.js
@@ -7,15 +7,15 @@ const app = express();
 
 app.use(cors());
 
-
+const APIKEY = 'RGAPI-7a0a67e0-9476-42be-8a98-ecb531eef305'
 const port = 4000
 
 app.get('/testAPI', async (req, res) => {
 
   const summonerName = req.query.summonerName;
-  const tagline = req.query.tagLine;
+  const tagLine = req.query.tagLine;
 
-  const puuid = await axios.get('https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + summonerName + '/' + tagLine + '?api_key=' + apiKey)
+  const puuid = await axios.get('https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + summonerName + '/' + tagLine + '?api_key=' + APIKEY)
     .then(response => {
         console.log(response.data.puuid);
         return response.data.puuid; 
